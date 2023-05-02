2bae22899a283abb522c87cb61201aa6e3d5f72d
Alex Conrey <alex.conrey@wellspring.com>
2023-05-01 21:52:41 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='vin_decoder_nhtsa',
+   version='0.1',
+   description='Python module for decoding VIN metadata from NHTSA API',
+   author='Man Foo',
+   author_email='foomail@foo.example',
+   packages=['vin_decoder_nhtsa'],  #same as name
+#    install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+)
---
+++ vin_decoder_nhtsa/decoder.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+class Vin():
+    VIN = None
+
+    def _search(self):
+        url = 'https://vpic.nhtsa.dot.gov/api//vehicles/DecodeVinValues/{}?format=json'.format(self.VIN)
+        resp = requests.get(url)
+        data = resp.json()
+        return data['Results']
+
+    def __init__(self, vin):
+        self.VIN = vin
+        data = self._search()
+        for k, v in data[0].items():
+            if v is '':
+                v = None
+            setattr(self, k, v)
+
+    def __str__(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
